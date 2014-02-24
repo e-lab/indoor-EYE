46c7a61cd0f39f018964bc89e1e0e892ea4bbbf7
Alfredo Canziani <alfredo.canziani@gmail.com>
2014-02-24 10:05:18 -0500

    Improved model definition function
    More general model (included stride for pooling)
    Renamed parameters for better understanding

--- Train/models.lua
+++ Train/models.lua
@@ -5,15 +5,16 @@
 function get_model1()
 
    --options for (conv+pool+threshold) layers
-   local nlayers = 1 --number of (conv+pool+threshold) layers
-   local nfeatures= {[0]=3, 32, 32} --number of feature maps in conv layers
-   local filtsizes = {9, 5} --filter sizes in conv layers
-   local paddings = {0, 0}
-   local strides = {1, 1}
-   local poolsizes = {2, 2}
+   local nConvLayers = 1 --number of (conv+pool+threshold) layers
+   local nFeatureMaps= {[0]=3, 32, 32} --number of feature maps in conv layers
+   local filterSize = {9, 5} --filter sizes in conv layers
+   local convPadding = {0, 0}
+   local convStride = {1, 1}
+   local poolSize = {2, 2}
+   local poolStride = {2, 2}
 
    --options for linear layers
-   local linears = {32} --number of neurons in linear layer
+   local neuronsPerLinearLayer = {32} --number of neurons in linear layer
 
    --neuralnet model consists of submodel1 and submodel2
    local model = nn.Sequential()
@@ -30,47 +31,47 @@ function get_model1()
    local nhiddens = {[0] = 0} --number of hidden units in layer i
    local nouts = {[0] = opt.width^2 * opt.ncolors} --number of output units in layer i
 
-   --add first 1..nlayers layers (conv+pool+threshold)
-   for i = 1, nlayers do
+   --add first 1..nConvLayers layers (conv+pool+threshold)
+   for i = 1, nConvLayers do
 
-      local conv_layer = {}
-      local pool_layer = {}
-      local test_batch = torch.Tensor(64, nfeatures[i - 1], mapsizes[i - 1], mapsizes[i - 1])
+      local convLayer = {}
+      local poolLayer = {}
+      local test_batch = torch.Tensor(64, nFeatureMaps[i - 1], mapsizes[i - 1], mapsizes[i - 1])
 
       if opt.cuda then
 
-         conv_layer = nn.SpatialConvolutionCUDA(nfeatures[i - 1], nfeatures[i], filtsizes[i], filtsizes[i], strides[i], strides[i], paddings[i])
-         pool_layer = nn.SpatialMaxPoolingCUDA(poolsizes[i], poolsizes[i], poolsizes[i], poolsizes[i])
-         conv_layer:cuda()
-         pool_layer:cuda()
+         convLayer = nn.SpatialConvolutionCUDA(nFeatureMaps[i - 1], nFeatureMaps[i], filterSize[i], filterSize[i], convStride[i], convStride[i], convPadding[i])
+         poolLayer = nn.SpatialMaxPoolingCUDA(poolSize[i], poolSize[i], poolStride[i], poolStride[i])
+         convLayer:cuda()
+         poolLayer:cuda()
          test_batch = nn.Transpose({1,4},{1,3},{1,2}):forward(test_batch):cuda()
 
       else
 
-         conv_layer = nn.SpatialConvolution(nfeatures[i - 1], nfeatures[i], filtsizes[i], filtsizes[i])
-         pool_layer = nn.SpatialMaxPooling(poolsizes[i], poolsizes[i], poolsizes[i], poolsizes[i])
+         convLayer = nn.SpatialConvolution(nFeatureMaps[i - 1], nFeatureMaps[i], filterSize[i], filterSize[i], convStride[i], convStride[i])
+         poolLayer = nn.SpatialMaxPooling(poolSize[i], poolSize[i], poolStride[i], poolStride[i])
 
       end
 
-      conv_layer.printable = true
-      conv_layer.text = 'Conv layer ' .. i
-      submodel1:add(conv_layer)
-      submodel1:add(pool_layer)
+      convLayer.printable = true
+      convLayer.text = 'Conv layer ' .. i
+      submodel1:add(convLayer)
+      submodel1:add(poolLayer)
       submodel1:add(nn.Threshold(0,0))
 
       --get layer sizes
-      local r1 = conv_layer:forward(test_batch)
-      local r2 = pool_layer:forward(r1)
---      print(#conv_layer.output)
---      print(#pool_layer.output)
-      mapsizes[i] = pool_layer.output:size(2)
-      nhiddens[i] = conv_layer.weight:size(2) * conv_layer.weight:size(3) * conv_layer.weight:size(4)
-      nconnections[i] = conv_layer.weight:size(1) * nhiddens[i] * (mapsizes[i - 1] - filtsizes[i] + 1) ^ 2
+      local r1 = convLayer:forward(test_batch)
+      local r2 = poolLayer:forward(r1)
+--      print(#convLayer.output)
+--      print(#poolLayer.output)
+      mapsizes[i] = poolLayer.output:size(2)
+      nhiddens[i] = convLayer.weight:size(2) * convLayer.weight:size(3) * convLayer.weight:size(4)
+      nconnections[i] = convLayer.weight:size(1) * nhiddens[i] * (mapsizes[i - 1] - filterSize[i] + 1) ^ 2
 
       if opt.cuda then
-         nouts[i] = pool_layer.output:size(2) * pool_layer.output:size(3) * nfeatures[i]
+         nouts[i] = poolLayer.output:size(2) * poolLayer.output:size(3) * nFeatureMaps[i]
       else
-         nouts[i] = pool_layer.output:size(3) * pool_layer.output:size(4) * nfeatures[i]
+         nouts[i] = poolLayer.output:size(3) * poolLayer.output:size(4) * nFeatureMaps[i]
       end
 
    end
@@ -81,28 +82,28 @@ function get_model1()
    end
 
    --reshape
-   submodel2:add(nn.Reshape(nouts[nlayers]))
+   submodel2:add(nn.Reshape(nouts[nConvLayers]))
 
    --add linear layers
-   for i = 1, #linears do
+   for i = 1, #neuronsPerLinearLayer do
 
-      nouts[nlayers + i] = linears[i]
-      local linear_layer = nn.Linear(nouts[nlayers + i - 1], nouts[nlayers + i])
+      nouts[nConvLayers + i] = neuronsPerLinearLayer[i]
+      local linear_layer = nn.Linear(nouts[nConvLayers + i - 1], nouts[nConvLayers + i])
       linear_layer.printable = true
       linear_layer.text = 'Linear layer ' .. i
       submodel2:add(linear_layer)
       submodel2:add(nn.Threshold(0, 0))
 
       --get layer sizes
-      mapsizes[nlayers + i] = 1
-      nhiddens[nlayers + i] = linears[i]
-      nconnections[nlayers + i] = nouts[nlayers + i - 1] * nouts[nlayers + i]
-      nfeatures[nlayers + i] = 1
+      mapsizes[nConvLayers + i] = 1
+      nhiddens[nConvLayers + i] = neuronsPerLinearLayer[i]
+      nconnections[nConvLayers + i] = nouts[nConvLayers + i - 1] * nouts[nConvLayers + i]
+      nFeatureMaps[nConvLayers + i] = 1
 
    end
 
    --add classifier
-   submodel2:add(nn.Linear(nouts[nlayers + #linears], #classes))
+   submodel2:add(nn.Linear(nouts[nConvLayers + #neuronsPerLinearLayer], #classes))
    --log probabilities
    submodel2:add(nn.LogSoftMax())
 
@@ -118,12 +119,12 @@ function get_model1()
       loss:cuda()
    end
 
-   --print sizes of nlayers
-   for i = 0, nlayers + #linears do
+   --print sizes of nConvLayers
+   for i = 0, nConvLayers + #neuronsPerLinearLayer do
 
       print(string.format(
       '==> model layer %2d  -  spatial extent: %3dx%3d  |  feature maps: %3d  |  hidden units: %4d  |  output size: %6d  |  connections: %8d',
-      i, mapsizes[i], mapsizes[i], nfeatures[i], nhiddens[i], nouts[i], nconnections[i]
+      i, mapsizes[i], mapsizes[i], nFeatureMaps[i], nhiddens[i], nouts[i], nconnections[i]
       ))
 
    end
