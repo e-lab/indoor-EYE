44c7b03c508454c0ffcdbc2b64fb87617f5e565b
Gregory Essertel <gregory.essertel@gmail.com>
2014-05-21 16:24:56 -0400

    ENHANCEMENT: optimise memory utilisation <loading_data_multi>
    Also fixbugs

--- Train/Data/load_data_multi.lua
+++ Train/Data/load_data_multi.lua
@@ -1,104 +1,119 @@
 function load_data_mm_multi(data_file, info_file)
 
    local dataset = {}
-   local nbThread = 2
+
+   -- (1) get the opt values...
+   local nbThreads = opt.loading_thread
+   local height = opt.height
+   local width = opt.width
+   local batchSize = opt.batchSize
+   local jitter = opt.jitter
+   local batchLength = batchSize*3*height*width
 
    -- (1) prepare shared data (as pointers)
-   -- (1.1) shuffle tensor
-   dataset.shuffle = torch.randperm(nsamples):type('torch.LongTensor')
+   -- (1.1) dataset information
+   dataset.info = torch.load(info_file)
+   local nSamples = dataset.info.labels:size(1)
+   local offsets_p     = torch.data(dataset.info.offsets, true)
+   local sizes_p       = torch.data(dataset.info.sizes, true)
+   local labels_p      = torch.data(dataset.info.labels, true)
+   local file_number_p = torch.data(dataset.info.file_number, true)
+
+   -- (1.2) shuffle tensor
+   dataset.shuffle = torch.randperm(nSamples):type('torch.LongTensor')
    local shuffle_p = tonumber(torch.data(dataset.shuffle, true))
 
-   -- (1.2) global mean and std
+   -- (1.3) global mean and std
    local gm_p = tonumber(torch.data(global_mean, true))
    local gstd_p = tonumber(torch.data(global_std, true))
 
-   -- (1.3) data destination (one for each thread)
+   -- (1.4) data destination (one for each thread)
    dataset.samples = {}
    dataset.targets = {}
-   dataset.samples_p = torch.LongTensor(nbThread)
-   dataset.targets_p = torch.LongTensor(nbThread)
-   for th = 1, nbThread do
-      dataset.samples[th] = torch.FloatTensor(opt.batchSize, opt.color, opt.height, opt.width)
-      samples_p           = tonumber(torch.data(samples[th], true))
-      dataset.targets[th] = torch.FloatTensor(opt.batchSize)
-      targets_p           = tonumber(torch.data(targets[th], true))
+   local samples_p = torch.LongTensor(nbThreads)
+   local targets_p = torch.LongTensor(nbThreads)
+   for th = 1, nbThreads do
+      dataset.samples[th] = torch.FloatTensor(opt.batchSize, 3, opt.height, opt.width)
+      samples_p[th]       = tonumber(torch.data(dataset.samples[th], true))
+      dataset.targets[th] = torch.LongTensor(opt.batchSize)
+      targets_p[th]       = tonumber(torch.data(dataset.targets[th], true))
    end
 
-   local samples_pp = tonumber(torch.data(dataset.samples_p, true))
-   local targets_pp = tonumber(torch.data(dataset.targets_p, true))
-
-   -- (1.4) dataset information
-   dataset.info = torch.load(info_file)
-   local offsets_p     = torch.data(dataset.info.offsets)
-   local sizes_p       = torch.data(dataset.info.sizes)
-   local labels_p      = torch.data(dataset.info.labels)
-   local file_number_p = torch.data(dataset.info.file_number)
+   local samples_pp = tonumber(torch.data(samples_p, true))
+   local targets_pp = tonumber(torch.data(targets_p, true))
 
    -- (1.5) images
    dataset.jpegs = {}
    local jpegs_p
    local nbFile = 1
-   if (infoDataset.file_range) then
+   if (dataset.info.file_range) then
       nbFile = dataset.info.file_range:size(1)
       jpegs_p = torch.LongTensor(nbFile)
       for file = 1, nbFile do
-         jpegs[file]   = torch.ByteStorage(data_file .. '-file' .. file .. '.t7')
-         jpegs_p[file] = tonumber(torch.data(jpegs[file], true))
+         dataset.jpegs[file]   = torch.ByteStorage(data_file .. '-file' .. file .. '.t7')
+         jpegs_p[file] = tonumber(torch.data(dataset.jpegs[file], true))
       end
    else
       jpegs_p = torch.LongTensor(nbFile)
-      jpegs[1] = torch.ByteStorage(data_file)
-      jpegs_p[1] = tonumber(torch.data(jpegs[1], true))
+      dataset.jpegs[1] = torch.ByteStorage(data_file)
+      jpegs_p[1] = tonumber(torch.data(dataset.jpegs[1], true))
    end
    local jpegs_pp = tonumber(torch.data(jpegs_p, true))
 
-   -- (2) prepare thread callback function
-   local function requirements()
+   --  (1) threads init functions
+   local function requirements ()
       ffi = require 'ffi'
-      gm = require 'graphicmagick'
+      gm = require 'graphicsmagick'
       mf = math.floor
+      require 'image'
    end
-
+   -- initData
    local function initData()
 
-      nSamples = dataset.info.labels:size(1)
-      c,h,w = opt.color, opt.height, opt.width
-      bs = opt.batchSize
-      batchLength = bs*c*h*w
-
+      c,h,w = 3, height, width
+      bs = batchSize
       -- (1) get shared data
       sharedShuffle_p = ffi.cast('unsigned long *', shuffle_p)
-      sharedGlobalMean_p = ffi.cast('unsigned long *', gm_p)
-      sharedGlobalStd_p = ffi.cast('unsigned long *', gstd_p)
+      sharedGlobalMean_p = ffi.cast('float *', gm_p)
+      sharedGlobalStd_p = ffi.cast('float *', gstd_p)
 
-      sharedSamples_pp = torch.LongTensor(torch.LongStorage(nbThread, samples_pp)):resize(nbThread)
-      sharedTargets_pp = torch.LongTensor(torch.LongStorage(nbThread, targets_pp)):resize(nbThread)
+      sharedSamples_pp = torch.LongTensor(torch.LongStorage(nbThreads, samples_pp)):resize(nbThreads)
+      sharedTargets_pp = torch.LongTensor(torch.LongStorage(nbThreads, targets_pp)):resize(nbThreads)
 
       -- (2) prepare dataset
       -- (2.1) making ffi pointer to useful informations
-      sharedOffsets_p     = ffi.cast('unsigned long *', offset_p)
+      sharedOffsets_p     = ffi.cast('unsigned long *', offsets_p)
       sharedSizes_p       = ffi.cast('unsigned long *', sizes_p)
       sharedLabels_p      = ffi.cast('unsigned long *', labels_p)
       sharedFile_number_p = ffi.cast('unsigned int *', file_number_p)
 
-      -- (2.3) pointer to the ByteStorage to different file
+      -- (2.3) pointer to the ByteStorage to different files
       local sharedJpegs_pp = torch.LongTensor(torch.LongStorage(nbFile, jpegs_pp)):resize(nbFile)
       sharedJpegs_p = {}
-      for (file = 1, nbFile) do
+      for file = 1, nbFile do
          sharedJpegs_p[file] = ffi.cast('unsigned char *', sharedJpegs_pp[file])
       end
 
+      -- (3) prepare data destination
+      -- samples = torch.FloatTensor(bs, c, h, w)
+      local sharedSamples_pp = torch.LongTensor(torch.LongStorage(nbThreads, samples_pp)):resize(nbThreads)
+      threadSamples = torch.FloatTensor(torch.FloatStorage(batchLength, sharedSamples_pp[threadId])):resize(bs, c, h, w)
+      threadSamples:zero()
+      -- targets = torch.LongTensor(bs)
+      local sharedTargets_pp = torch.LongTensor(torch.LongStorage(nbThreads, targets_pp)):resize(nbThreads)
+      threadTargets_p = ffi.cast('unsigned long *', sharedTargets_pp[threadId])
+
    end
 
-   dataset.jobToDo = function (threadId, idx, test)
+   -- (2) prepare job function
+   dataset.jobToDo = function (tId, idx, test)
+      assert(tId == threadId)
 
-      -- (1) get data destination
-      sample = torch.FloatTensor(torch.FloatStorage(batchLength, sharedSamples_pp[threadId])):resize(bs, c, h, w)
-      sample:zero()
-      target_p = ffi.cast('unsigned float *', sharedTargets_p[threadId])
+      threadSamples:zero()
 
       -- (2) process batch:
       for i = 0,bs-1 do
+
          -- offsets:
          local start = ((idx-1)*bs + i) % nSamples
          local ii = sharedShuffle_p[start] - 1
@@ -136,31 +151,30 @@ function load_data_mm_multi(data_file, info_file)
 
          -- normalize sample
          for j = 1, c do
-            sample[j]:add(-global_mean[j])
-            sample[j]:div(global_std[j])
+            sample[j]:add(-sharedGlobalMean_p[j-1])
+            sample[j]:div(sharedGlobalStd_p[j-1])
          end
 
          -- save sample:
-         samples[i+1] = sample
+         threadSamples[i+1] = sample
 
          -- label:
-         targets_p[i] = labels_p[ii]
+         threadTargets_p[i] = sharedLabels_p[ii]
       end
 
-      return
+      return threadId
    end
 
-   dataset.finishJob = function finishJob()
+   dataset.finishJob = function(tId)
    end
-
    -- (3) create threads
-   dataset.threads = require('MyThreads')(nbThreadb, requirements, initData)
+   dataset.threads = require('Data/MyThreads')(nbThreads, requirements, initData)
 
    -- (4) prepare main thread functions
-   dataset.nBatches = math.floor(nsamples / bs)
+   dataset.nBatches = math.floor(nSamples / batchSize)
    dataset.threadId = torch.IntTensor(dataset.nBatches)
 
-   dataset.preparebatch = function(t, test)
+   dataset.prepareBatch = function(t, test)
       dataset.threadId[t] = dataset.threads:addjob(dataset.jobToDo, dataset.finishJob, t, test)
    end
 
@@ -171,12 +185,12 @@ function load_data_mm_multi(data_file, info_file)
          tgs:copy(dataset.targets[dataset.threadId[t]])
          dataset.threadId[t] = 0
       else
-         error(string.format("Thread %d hasn't be prepared.", t))
+         error(string.format("The batch %d hasn't be prepared.", t))
       end
    end
 
    dataset.newShuffle = function()
-      dataset.shuffle:copy(torch.randperm(nsamples):type('torch.LongTensor'))
+      dataset.shuffle:copy(torch.randperm(nSamples):type('torch.LongTensor'))
    end
 
 
