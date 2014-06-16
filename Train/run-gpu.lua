1554e89952c566fdce816e0841e23b14d01cb959
GregoryEssertel <gesserte@purdue.edu>
2014-06-16 17:17:53 -0400

    New <run-gpu> and <train-and-test-gpu>
    Add two new script cleaner and only for GPU

---
+++ Train/run-gpu.lua
@@ -0,0 +1,185 @@
+-------------------------------------------------------------------------------
+-- Main script for training neural network
+-- Gregory Essertel, June 2014
+-- based on the work of:
+-- Artem Kuharenko
+-- Alfredo Canziani, Feb 2014
+-- Gregory Essertel, May 2014
+-------------------------------------------------------------------------------
+
+-- Require packages -----------------------------------------------------------
+require 'torch'
+require 'nnx'
+require 'net-toolkit'
+ffi = require 'ffi'
+require 'pl'
+require 'sys'
+require 'cutorch'
+require 'cunn'
+require 'dataset-manager'
+
+-- Title definition -----------------------------------------------------------
+local title = [[
+_           _                         _   _ ______ _______
+(_)         | |                       | \ | |  ____|__   __|
+_ _ __   __| | ___   ___  _ __ ______|  \| | |__     | |
+| | '_ \ / _` |/ _ \ / _ \| '__|______| . ` |  __|    | |
+| | | | | (_| | (_) | (_) | |         | |\  | |____   | |
+|_|_| |_|\__,_|\___/ \___/|_|         |_| \_|______|  |_|
+
+]]
+
+-- Options --------------------------------------------------------------------
+opt = lapp(title .. [[
+
+Dataset's parameters
+--side            (default 128)        Training and testing image's side length (max 256)
+--colour          (default true)       True by default, allows to train on B&W if the flag is called
+--jitter          (default 0)          Introduce random crop for loweing overfitting
+--mmload                               Memory mapping when loading data. Use with small RAM
+--mm_threads      (default 4)          Number of threads use for mm
+--dropout         (default 0)          Dropout in MLP. Set it to 0 for disabling it, 0.5 for "standard" working value
+--inputDO         (default 0)          Input dropout. Set it to 0 for disabling it, 0.2 for "standard" working value
+--subsample_name  (default 'indoor51') Name of imagenet subsample. Possible options ('class51', 'elab')
+
+Learning parameters
+--learningRate      (default 5e-2)
+--learningRateDecay (default 1e-7)
+--weightDecay       (default 0   )
+--momentum          (default 0   )
+--batchSize         (default 128 )
+
+On screen output
+--plot                                  Plot training and testing accuracies
+--verbose                (default true) Show more output on screen
+
+Output folder
+--save_dir (default './results/') Folder where the stats and models are saved
+
+CUDA parameters
+--devid (default) device ID (if using CUDA)
+
+Other parameters
+--num_threads       (default 6   )
+--probe                            Prints to screen feature maps stats after every training iteration
+--network           (default N/A ) Load specified net from file. Default is "Not Available" -> new net is generated
+]])
+
+torch.setdefaulttensortype('torch.FloatTensor')
+
+-- Aggiusting options ---------------------------------------------------------
+-- Title ----------------------------------------------------------------------
+if opt.verbose then
+   print(title)
+
+   -- Print options summary ------------------------------------------------------
+   print('==> Options:')
+   for a,b in pairs(opt) do print('     + ' .. a .. ':', b) end
+   print()
+end
+
+-- Aggiusting options ---------------------------------------------------------
+if opt.colour then
+   opt.ncolors = 3
+else
+   opt.ncolors = 1
+end
+
+-- Training with GPU ----------------------------------------------------------
+if opt.verbose then
+   print('==> switching to CUDA')
+end
+cutorch.setDevice(opt.devid)
+cutorch.deviceReset()
+if opt.verbose then
+   print('==> using GPU #' .. cutorch.getDevice())
+   print(cutorch.getDeviceProperties(opt.devid))
+end
+
+-- Loading functions ----------------------------------------------------------
+if opt.verbose then
+   print('==> Loading <training> and <testing> functions')
+end
+dofile('train-and-test-gpu.lua')
+
+if opt.verbose then
+   print('==> Loading <model> functions')
+end
+dofile('models.lua')
+if opt.verbose then
+   print('==> Loading <data> functions')
+end
+
+torch.manualSeed(123)
+torch.setnumthreads(opt.num_threads) --some of data scripts may change numthreads, so we need to do it here
+
+print('Currently using ' .. torch.getnumthreads() .. ' threads')
+os.execute('mkdir -p ' .. opt.save_dir) --create folder for saving results
+
+-- Datasets --------------------------------------------------------------------
+local trainOpt = {}
+trainOpt.side = opt.side
+trainOpt.jitter = opt.jitter
+trainOpt.batchSize = opt.batchSize
+trainOpt.verbose = true
+trainOpt.nbThreads = 1
+trainOpt.normInput = true
+
+local datasetExtractor = dmanager.TrainingExtractorAsyn(opt.subsample_name, trainOpt)
+
+--print train and test image sizes
+if verbose then
+   print(string.format('==> Train number of batches: %d, Batch size: %dx%dx%dx%d', datasetExtractor.getNbBatches(true), opt.colour, opt.side, opt.side))
+   print(string.format('==> Test number of batches: %d, Batch size: %dx%dx%dx%d',  datasetExtractor.getNbBatches(false), opt.colour, opt.side, opt.side))
+end
+--compute global mean and std
+local stats = datasetExtractor:getGlobalMeanAndStd(opt.side, 8, opt.verbose)
+
+-- Loggers ----------------------------------------------------------------------
+print '==> Set up Log'
+local statFile
+if (opt.network == 'N/A') then
+   -- Open file in re-write mode (NOT append)
+   statFile = io.open(opt.save_dir .. 'stat.txt','w+')
+   -- Writing title
+   statFile:write(title)
+else
+   statFile = io.open(opt.save_dir .. 'stat.txt','a')
+
+   statFile:write('\n')
+   statFile:write('-------------------------------------------------------------------------------\n')
+   statFile:write('------------------------------------ Restart ----------------------------------\n')
+   statFile:write('-------------------------------------------------------------------------------\n')
+end
+-- Writing current commit hash
+statFile:write(string.format('Current commit hash: %s\n',sys.execute('git rev-parse HEAD')))
+-- Collecting input arguments and writing them to file
+local inputArg = ''
+for i = 1,#arg do
+   inputArg = inputArg .. ' ' .. arg[i];
+end
+statFile:write(string.format('User command line input:%s\n',inputArg))
+statFile:flush()
+
+-- Training and testing --------------------------------------------------------
+print '==> Start Training'
+
+--get classifier and loss function
+local model, logsoft, loss, dropout
+if opt.network == 'N/A' then
+   model, logsoft, loss, dropout = get_model1(true) --(classifier.lua)
+   local tmpFile = io.open('.pltStat', 'r')
+   statFile:write(tmpFile:read('*all'))
+   io.close(tmpFile)
+   os.execute('rm .pltStat .pltStatData')
+else
+   if (verbose) then
+      print('Loading network from file: ' .. opt.network)
+   end
+   model, logsoft, loss, dropout = get_model2(opt.network)
+end
+collectgarbage() -- get rid of craps from the GPU's RAM
+
+--train classifier
+train_and_test(datasetExtractor, model, logsoft, loss, dropout) --(train-and-test.lua)
+-------------------------------------------------------------------------------
---
+++ Train/train-and-test-gpu.lua
@@ -0,0 +1,342 @@
+-------------------------------------------------------------------------------
+-- Functions for training and testing a neural network
+-- Gregory Essertel, June 2014
+-- based on the work of:
+-- Artem Kuharenko
+-- Alfredo Canziani, Feb 2014
+-- Gregory Essertel, May 2014
+-------------------------------------------------------------------------------
+
+require 'torch'   -- torch
+require 'optim'   -- an optimization package, for online and batch methods
+local TopAcc = require('TopAccuracy')
+
+--allocate memory for batch of images
+local ims = torch.Tensor(opt.batchSize, opt.colour, opt.side, opt.side)
+ims:cuda()
+
+--allocate memory for batch of labels
+local targets = torch.Tensor(opt.batchSize)
+
+-- w and dE_dw are global for this file
+local w, dE_dw
+local optimState
+
+function train(datasetExtractor, model, logsoft, loss, dropout, top5)
+   --train one iteration
+   local nbThread = datasetExtractor:getNbThreads()
+   datasetExtractor:newShuffle(true)
+
+   for batch = 1, nbThread do
+      datasetExtractor:prepareBatch(batch, true)
+   end
+
+   local ce_train_error = 0
+   local loading_time = 0
+   local computing_time = 0
+   local nb_batches = datasetExtractor:getNbBatches(true)
+
+   for batch = 1, nb_batches do
+
+      xlua.progress(batch, nb_batches)
+
+      --copy batch
+      local timerLoading = torch.Timer()
+      datasetExtractor:copyBatch(batch, ims, targets)
+      loading_time = loading_time + timerLoading:time().real
+
+      --prepare next batch
+      if batch + nbThread <= nb_batches then
+         datasetExtractor:prepareBatch(batch + nbThread, true)
+      end
+
+      -- create closure to evaluate f(X) and df/dX
+      local eval_E = function (att)
+
+         dE_dw:zero()
+
+         local outputModelGPU = model:forward(ims)
+         cutorch.synchronize()
+         local outputModelCPU = outputModelGPU:float()
+         local preds = logsoft:forward(outputModelCPU)
+         local E = loss:forward(preds, targets)
+
+         top5:batchAdd(preds, targets)
+         ce_train_error = ce_train_error + E
+
+         local dE_dy = loss:backward(preds, targets)
+         local gradLogSoftCPU = logsoft:backward(outputModelCPU, dE_dy)
+
+         -- on GPU
+         local gradLogSoftGPU = gradLogSoftCPU:cuda()
+         model:backward(ims, gradLogSoftGPU)
+         cutorch.synchronize()
+
+         return E, dE_dw
+      end
+
+      -- optimize on current mini-batch
+      collectgarbage()
+      local timerComputing = torch.Timer()
+      optim.sgd(eval_E, w, optimState)
+      computing_time = computing_time + timerComputing:time().real
+   end
+
+   loading_time = loading_time / nb_batches
+   computing_time = computing_time / nb_batches
+   ce_train_error = ce_train_error / nb_batches
+
+   return ce_train_error, loading_time, computing_time
+end
+
+function test(datasetExtractor, model, logsoft, loss, dropout, top5)
+
+   local nbThread = datasetExtractor:getNbThreads()
+   datasetExtractor:newShuffle(false)
+
+   for batch = 1, nbThread do
+      datasetExtractor:prepareBatch(batch, false)
+   end
+
+   local ce_test_error = 0
+   local loading_time = 0
+   local computing_time = 0
+   local nb_batches = datasetExtractor:getNbBatches(false)
+
+
+   -- Switching off the dropout
+   if opt.dropout > 0 or opt.inputDO > 0 then
+      for _,d in ipairs(dropout) do
+         d.train = false
+      end
+   end
+
+   for batch = 1, nb_batches do
+
+      xlua.progress(batch, data.nBatches)
+
+      local timerLoading = torch.Timer()
+      data.copyBatch(batch, ims, targets)
+      loading_time = loading_time + timerLoading:time().real
+      --prepare next batch
+      if batch + nbThread <= data.nBatches then
+         data.prepareBatch(batch + nbThread, false)
+      end
+
+      -- test sample
+      local timerComputing = torch.Timer()
+      local outputModelGPU = model:forward(ims)
+      cutorch.synchronize()
+      local outputModelCPU = outputModelGPU:float()
+      local preds = logsoft:forward(outputModelCPU)
+      local E = loss:forward(preds, targets)
+
+      top5:batchAdd(preds, targets)
+      ce_test_error = ce_test_error + E
+
+      computing_time = computing_time + timerComputing:time().real
+   end
+
+   -- Switching back on the dropout
+   if opt.dropout > 0 or opt.inputDO > 0 then
+      for _,d in ipairs(dropout) do
+         d.train = true
+      end
+   end
+
+   ce_test_error = ce_test_error / nb_batches
+   loading_time = loading_time / nb_batches
+   computing_time = computing_time / nb_batches
+
+   return ce_test_error, loading_time, computing_time
+end
+
+local function recovert_logger(epochInit)
+   local tmpAcc = {}
+   local tmpCE = {}
+   local tmpAcc5 = {}
+   if (paths.filep(opt.save_dir .. 'accuracy.log')) then
+      local file = io.open(opt.save_dir .. 'accuracy.log', r)
+      local tmpVal = file:read("*line")  -- skip the header
+      for ep = 1, epochInit do
+         tmpVal = file:read("*number")  -- train
+         table.insert(tmpAcc, tmpVal)
+         tmpVal = file:read("*number")  -- test
+         table.insert(tmpAcc, tmpVal)
+      end
+      io.close(file)
+   end
+   if (paths.filep(opt.save_dir .. 'cross-entropy.log')) then
+      local file = io.open(opt.save_dir .. 'cross-entropy.log', r)
+      local tmpVal = file:read("*line")  -- skip the header
+      for ep = 1, epochInit do
+         tmpVal = file:read("*number")  -- train
+         table.insert(tmpCE, tmpVal)
+         tmpVal = file:read("*number")  -- test
+         table.insert(tmpCE, tmpVal)
+      end
+      io.close(file)
+   end
+   if (paths.filep(opt.save_dir .. 'accuracy-5.log')) then
+      local file = io.open(opt.save_dir .. 'accuracy-5.log', r)
+      local tmpVal = file:read("*line")  -- skip the header
+      for ep = 1, epochInit do
+         for log = 1, 2 do
+            tmpVal = file:read("*number")  -- train
+            table.insert(tmpAcc5, tmpVal)
+            tmpVal = file:read("*number")  -- test
+            table.insert(tmpAcc5, tmpVal)
+         end
+      end
+      io.close(file)
+   end
+
+   -- (3) init loggers
+   local logger = optim.Logger(opt.save_dir .. 'accuracy.log')
+   local ce_logger = optim.Logger(opt.save_dir .. 'cross-entropy.log')
+   local logger_5 = optim.Logger(opt.save_dir .. 'accuracy-5.log')
+   logger_5:setNames({'% train top5', '% test top5',
+   '% train top1', '% test top1'})
+
+   -- (4) load backup data
+   for ep = 1, epochInit do
+      logger:add{['% train accuracy'] = tmpAcc[2*ep-1], ['% test accuracy'] = tmpAcc[2*ep]}
+      ce_logger:add{['ce train error'] = tmpCE[2*ep-1], ['ce test error'] = tmpCE[2*ep]}
+      logger_5:add{tmpAcc5[4*ep-3], tmpAcc5[4*ep-2],
+      tmpAcc5[4*ep-1], tmpAcc5[4*ep]}
+   end
+
+   return logger, ce_logger, logger_5
+end
+
+function train_and_test(dataset, model, logsoft, loss, dropout)
+
+   w, dE_dw = model:getParameters()
+
+   --init confusion matricies
+   local nb_classes = #dataset:getClasses()
+   local train_top5 = TopAcc(nb_classes, 5)
+   local test_top5 = TopAcc(nb_classes, 5)
+   train_top5:zero()
+   test_top5:zero()
+
+   --set optimization parameters
+   optimState = {
+      learningRate = opt.learningRate,
+      momentum = opt.momentum,
+      weightDecay = opt.weightDecay,
+      learningRateDecay = opt.learningRateDecay
+   }
+
+   --init logger for train and test accuracy
+   local logger
+   --init logger for train and test cross-entropy error
+   local ce_logger
+   --init logger for train and test accuracy-5
+   local logger_5
+
+   local epochInit = 0
+   local epoch = 1
+   -- recover logger if restaring the training
+   if opt.network ~= 'N/A' then
+      -- (1) get the number of the epoch
+      epochInit = tonumber(string.match(opt.network, "%d+"))
+      epoch = epochInit + 1
+
+      -- (2) recover loggers
+      logger, ce_logger, logger_5 = recover_loggers(epochInit)
+
+      -- (3) plot
+      if opt.plot then
+         logger:style{['% train accuracy'] = '-', ['% test accuracy'] = '-'}
+         logger:plot()
+         ce_logger:style{['ce train error'] = '-', ['ce test error'] = '-'}
+         ce_logger:plot()
+         logger_5:style{'-', '-', '-', '-'}
+         logger_5:plot()
+      end
+   else
+      logger = optim.Logger(opt.save_dir .. 'accuracy.log')
+      ce_logger = optim.Logger(opt.save_dir .. 'cross-entropy.log')
+      logger_5 = optim.Logger(opt.save_dir .. 'accuracy-5.log')
+      logger_5:setNames({'% train top5', '% test top5',
+      '% train top1', '% test top1'})
+      logger:style{['% train accuracy'] = '-', ['% test accuracy'] = '-'}
+      ce_logger:style{['ce train error'] = '-', ['ce test error'] = '-'}
+      logger_5:style{'-', '-', '-', '-'}
+   end
+
+   local continue = true
+
+   local total_timer = torch.Timer()
+   while continue do
+      collectgarbage()
+      -------------------------------------------------------------------------------
+      --train
+      if opt.verbose then
+         print('==> Train ' .. epoch)
+      end
+      local train_timer = torch.Timer()
+      local train_ce_error, train_loading, train_computing = train(dataset, model, logsoft, loss, dropout, train_top5)
+      local train_time = train_timer:clock().real
+
+      -- (2) testing
+      if opt.verbose then
+         print('==> Test ' .. epoch)
+      end
+      local test_timer = torch.Timer()
+      local test_ce_error, test_loading, test_computing = test(dataset, model, loss, logsoft, dropout, test_top5)
+      local test_time = test_tiner:clock().real
+
+      -- (3) update loggers
+      train_top5:update()
+      test_top5:update()
+      logger:add{['% train accuracy'] = train_top5.result[1] * 100, ['% test accuracy'] = test_top5.result[1] * 100}
+      ce_logger:add{['ce train error'] = ce_train_error, ['ce test error'] = ce_test_error}
+      logger_5:add{train_top5.result[5] * 100,test_top5.result[5] * 100,
+      train_top5.result[1] * 100, test_top5.result[1] * 100}
+
+      local comment = string.format([[%s\n%s\n%s\n%s\n%s\n%s\n\n%s\n%s\n%s\n%s\n\n%s\n]],
+      string.format('------------------------ Statistics epoch %3d ------------------------', epoch),
+      'Accuracy\tTrain\t\tTest',
+      string.format("Top5\t%.2f\t\t%.2f", train_top5.result[5] * 100, test_top5.result[5] * 100),
+      string.format("Top1\t%.2f\t\t%.2f", train_top5.result[1] * 100, test_top5.result[1] * 100),
+      'CE error\tTrain\t\tTest',
+      string.format("\t\t%.4f\t\t%.4f", train_ce_error, test_ce_error),
+      'Times\t\t\tTrain\t\tTest',
+      string.format("Epoch(min)\t\t%.2f\t\t%.2f", train_time/60, test_time / 60),
+      string.format("Batch Loading(msec)\t%.3f\t\t%.3f", train_loading * 1000, test_loading * 1000),
+      string.format("Batch Computing(msec)\t%.3f\t\t%.3f", train_computing* 1000, test_computing * 1000),
+      string.format("Total time(min)\t%.2f", total_timer:clock().real/60))
+
+      if (opt.verbose) then
+         print(comment)
+      end
+
+      train_top5:zero()
+      test_top5:zero()
+
+      if opt.plot then
+         logger:plot()
+         ce_logger:plot()
+         logger_5:plot()
+      end
+
+      -- (4) save the network
+      w = nil
+      dE_dw = nil
+      w, dE_dw = netToolkit.saveNet(model, opt.save_dir .. 'model-' .. epoch .. '.net', opt.verbose)
+
+      -- (5) log in file
+      statFile:write(comment)
+      statFile:flush()
+
+      -- (6) set up for the next epoch
+      epoch = epoch + 1
+
+      if (paths.filep('./.stop')) then
+         os.execute 'rm .stop'
+         continue = false
+      end
+   end
+end
