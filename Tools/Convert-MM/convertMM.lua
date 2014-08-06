8aae7f66dca9248fc05d80c85b764b24e132c599
Alfredo Canziani <alfredo.canziani@gmail.com>
2014-08-06 10:52:47 -0400

    Typos in <convertMM>

--- Tools/Convert-MM/convertMM.lua
+++ Tools/Convert-MM/convertMM.lua
@@ -1,10 +1,9 @@
 --[[
     Author: Aysegul Dundar (adundar@purdue.edu)
     This file converts GPU trained ConvolutionMM module to Spatial Convolution
-    Because ConvolutionMM for GPU has the ability of having stride more than 1 
-    whereas ConvolutionMM for CPU not. 
+    Because ConvolutionMM for GPU has the ability of having stride more than 1
+    whereas ConvolutionMM for CPU has not.
 ]]
-
 
 require 'cutorch'
 require 'cunn'
@@ -12,13 +11,13 @@ require 'nnx'
 
 local network = torch.load('model.net'):float()
 
--- stride pre-defoned
+-- stride pre-defined
 local dW = 4
-local dH = 4 
+local dH = 4
 
 local m_tmp = network.modules[1].modules[1]
 local conv_tmp = nn.SpatialConvolution(m_tmp.nInputPlane, m_tmp.nOutputPlane, m_tmp.kW, m_tmp.kH, dW, dH)
-conv_tmp.weight = m_tmp.weight:reshape(m_tmp.nOutputPlane, m_tmp.nInputPlane, m_tmp.kW, m_tmp.kH) 
+conv_tmp.weight = m_tmp.weight:reshape(m_tmp.nOutputPlane, m_tmp.nInputPlane, m_tmp.kW, m_tmp.kH)
 conv_tmp.bias   = m_tmp.bias
 
 network.modules[1].modules[1] = conv_tmp
