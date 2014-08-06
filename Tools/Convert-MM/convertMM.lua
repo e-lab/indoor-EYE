2e21cddfae99ced874b732b6593b2c05ac4df6c7
Alfredo Canziani <alfredo.canziani@gmail.com>
2014-08-06 10:58:37 -0400

    <convertMM> is now a script
    To run it: `./convertMM.lua pathToModel.net`

--- Tools/Convert-MM/convertMM.lua
+++ Tools/Convert-MM/convertMM.lua
@@ -1,15 +1,19 @@
+#!/usr/bin/env th
 --[[
     Author: Aysegul Dundar (adundar@purdue.edu)
     This file converts GPU trained ConvolutionMM module to Spatial Convolution
     Because ConvolutionMM for GPU has the ability of having stride more than 1
     whereas ConvolutionMM for CPU has not.
 ]]
+-- Alfredo Canziani, Aug 2014
 
 require 'cutorch'
 require 'cunn'
 require 'nnx'
 
-local network = torch.load('model.net'):float()
+if not arg[1] then print "Network unspecified (type it after the program's name)" return
+else print('Loading: ' .. arg[1]) end
+local network = torch.load(arg[1]):float()
 
 -- stride pre-defined
 local dW = 4
